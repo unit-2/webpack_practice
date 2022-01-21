b8592e79b7cf42d951498cf42d6f57c7a0f3d4ce
unit2 <43838694+unit-2@users.noreply.github.com>
2022-01-21 17:56:58 +0900

    Generate HTML through Pug

--- webpack.config.js
+++ webpack.config.js
@@ -51,8 +51,22 @@ module.exports = {
 							name: 'images/[name].[ext]',
 						},
 					},
-				]
+				],
 			},
+			{
+				test: /\.pug/,
+				use: [
+					{
+						loader: 'html-loader'
+					},
+					{
+						loader: 'pug-html-loader',
+						options: {
+							pretty: true,
+						},
+					},
+				],
+			}
 		],
 	},
 	  // プラグイン追加
@@ -61,7 +75,12 @@ module.exports = {
 			filename: './stylesheets/main.css',
 		}),
 		new HtmlWebpackPlugin({
-			template: './src/templates/index.html',
+			template: './src/templates/index.pug',
+			filename: 'index.html',
+		}),
+		new HtmlWebpackPlugin({
+			template: './src/templates/access.pug',
+			filename: 'access.html',
 		}),
 		new CleanWebpackPlugin(),
   ],
