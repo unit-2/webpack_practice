3a02132cfd5be440093a7de66c857ac50620221a
unit2 <43838694+unit-2@users.noreply.github.com>
2022-01-24 16:42:37 +0900

    Add Babel

--- dist/javascripts/main.js
+++ dist/javascripts/main.js
@@ -6,27 +6,17 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-/******/ (function() { // webpackBootstrap
+/******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
-
-/***/ "./src/stylesheets/main.scss":
-/*!***********************************!*\
-  !*** ./src/stylesheets/main.scss ***!
-  \***********************************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
-
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://webpack-practice/./src/stylesheets/main.scss?");
-
-/***/ }),
 
 /***/ "./src/javascript/main.js":
 /*!********************************!*\
   !*** ./src/javascript/main.js ***!
   \********************************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _my_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./my.js */ \"./src/javascript/my.js\");\n/* harmony import */ var _stylesheets_main_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../stylesheets/main.scss */ \"./src/stylesheets/main.scss\");\n//自分で作ったモジュールを読み込む\n\n//モジュールとしてcssファイルをjsに読み込む\n\n\nconsole.log('This is index.js');\n(0,_my_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n\n\n//# sourceURL=webpack://webpack-practice/./src/javascript/main.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _my_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./my.js */ \"./src/javascript/my.js\");\n/* harmony import */ var _stylesheets_main_scss__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../stylesheets/main.scss */ \"./src/stylesheets/main.scss\");\n//自分で作ったモジュールを読み込む\n //モジュールとしてcssファイルをjsに読み込む\n\n\nconsole.log('This is index.js');\n(0,_my_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])();\n\n//# sourceURL=webpack://webpack-practice/./src/javascript/main.js?");
 
 /***/ }),
 
@@ -34,9 +24,19 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _my_
 /*!******************************!*\
   !*** ./src/javascript/my.js ***!
   \******************************/
-/***/ (function(__unused_webpack_module, __webpack_exports__, __webpack_require__) {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ __webpack_exports__[\"default\"] = (() => {\n\tconsole.log('this is module');\n});\n\n\n//# sourceURL=webpack://webpack-practice/./src/javascript/my.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nfunction ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }\n\nfunction _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = null != arguments[i] ? arguments[i] : {}; i % 2 ? ownKeys(Object(source), !0).forEach(function (key) { _defineProperty(target, key, source[key]); }) : Object.getOwnPropertyDescriptors ? Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)) : ownKeys(Object(source)).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } return target; }\n\nfunction _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (function () {\n  var obj = {\n    a: 1,\n    b: 2\n  };\n\n  var newObj = _objectSpread(_objectSpread({}, obj), {}, {\n    c: 3\n  }); // { a:1, b:2, c:3 }\n\n\n  console.log('this is module', newObj);\n});\n\n//# sourceURL=webpack://webpack-practice/./src/javascript/my.js?");
+
+/***/ }),
+
+/***/ "./src/stylesheets/main.scss":
+/*!***********************************!*\
+  !*** ./src/stylesheets/main.scss ***!
+  \***********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://webpack-practice/./src/stylesheets/main.scss?");
 
 /***/ })
 
@@ -67,16 +67,33 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony default export */ 
 /******/ 	}
 /******/ 	
 /************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
-/******/ 	!function() {
+/******/ 	(() => {
 /******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = function(exports) {
+/******/ 		__webpack_require__.r = (exports) => {
 /******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
 /******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
 /******/ 			}
 /******/ 			Object.defineProperty(exports, '__esModule', { value: true });
 /******/ 		};
-/******/ 	}();
+/******/ 	})();
 /******/ 	
 /************************************************************************/
 /******/ 	
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+//const path = require('path'); 『output: 』には絶対パスが必要なため、こちらの記述が必要となる
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 //HTMLファイルも自動で出力する
@@ -20,6 +21,24 @@ module.exports = {
 		//ルールという配列を追加
 		rules: [
 			{
+				//JSに関する設定
+				//babelを使ってES6をトランスパイルする
+				test: /\.js/,
+				//node_modulesの中にあるファイルは対象外にする
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+							presets: [
+								//optionの中にさらにoptionを配列で追加 トランスパイルするかブラウザを指定する
+								['@babel/preset-env', { "targets": "> 0.25%, not dead" }],
+							]
+            },
+          },
+        ],
+      },
+			{
 				//配列を追加  バックスラッシュでドットをエスケープ正規表現(//で囲まれた書き方をする）
 				//.css .scss .sassを検出する指示
 				test: /\.(css|scss|sass)$/,
@@ -28,14 +47,18 @@ module.exports = {
 					//注意、loaderは下から適応される
 					{
 					//読み込んだモジュールを下記で処理 cssをスタイルで読み込む
+						//CSSを適用
 						//loader: 'style-loader',
-						// loader: 'style-loader', 削除
-            loader: MiniCssExtractPlugin.loader, // ローダーを別のを追加
+						//style-loaderの代わりにMiniCssExtractPlugin.loaderが役割を果たす
+            loader: MiniCssExtractPlugin.loader,
 					},
 					//スタイルを適応
+					//読み込んだCSS
 					{
 						loader: 'css-loader',
 						options: {
+							//scssのソースマップ(※ソースマップを出力すると重くなるので開発のとき以外はfalseにしておくとよい
+							//sourceMap: true, //開発のときのみtrue
 							// オプションでCSS内のurl()メソッドの取り込みを禁止する
 							url: false,
 						}
@@ -58,10 +81,10 @@ module.exports = {
 					// Sassをバンドルするための機能
 					{
 						loader: 'sass-loader',
-						options: {
-              // ソースマップの利用有無
-              sourceMap: true,
-            },
+						// options: {
+            //   // ソースマップの利用有無
+            //   sourceMap: true,
+            // },
           },
 				],
 			},
@@ -101,7 +124,8 @@ module.exports = {
 			}
 		],
 	},
-	  // プラグイン追加
+	// プラグイン追加
+	//注意: 並列関係,「plugin:」ではなくplugins:
   plugins: [
 		new MiniCssExtractPlugin({
 			filename: './stylesheets/main.css',
